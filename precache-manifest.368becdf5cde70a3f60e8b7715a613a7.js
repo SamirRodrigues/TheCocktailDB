02bca9e9e1a906faf105c4d9a4a96c78d4af5f8d
SamirRodrigues <emaildesamir@gmail.com>
2021-12-31 17:31:33 -0300

    Updates

---
+++ precache-manifest.368becdf5cde70a3f60e8b7715a613a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6e1271d774ff9eb0195eabb24acb4c",
+    "url": "/TheCocktailDB/index.html"
+  },
+  {
+    "revision": "291b28263c656512b95d",
+    "url": "/TheCocktailDB/static/css/main.7c0f243b.chunk.css"
+  },
+  {
+    "revision": "4f6a224e92298720f10b",
+    "url": "/TheCocktailDB/static/js/2.22f37304.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TheCocktailDB/static/js/2.22f37304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "291b28263c656512b95d",
+    "url": "/TheCocktailDB/static/js/main.b8e3270f.chunk.js"
+  },
+  {
+    "revision": "ee140066d2e8afceeb77",
+    "url": "/TheCocktailDB/static/js/runtime-main.aaa049b5.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/TheCocktailDB/static/media/logo.9a3d2645.svg"
+  }
+]);
